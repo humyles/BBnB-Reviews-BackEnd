740ec5baebb7d0c615473fcfc29bfcd354ae31d8
chair <myleshu@gmail.com>
2020-03-03 14:42:45 -0500

    update

--- reviews-module/server/router.js
+++
@@ -1,6 +0,0 @@
-var reviewsRouter = require('express').Router();
-var reviewsController = require('./controller');
-
-reviewsRouter.get('/api/items/:listingId', reviewsController.retrieve);
-
-module.exports = reviewsRouter;
